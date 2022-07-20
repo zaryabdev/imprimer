39abb02f8c19f217fcd03f275ecc31ab1fca767f
zaryabdev <zaryabdev404@gmail.com>
2022-07-20 18:14:44 +0500

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,17 @@ module.exports = {
         path: path.resolve(__dirname, "app/dist"),
         filename: "bundle.js"
     },
+    module: {
+        rules: [
+            // loads .js/jsx/json files
+            {
+                test: /\.jsx?$/,
+                include: [path.resolve(__dirname, "app/src")],
+                loader: "babel-loader",
+                resolve: {
+                    extensions: [".js", ".jsx", ".json"]
+                }
+            }
+        ]
+    }
 };
